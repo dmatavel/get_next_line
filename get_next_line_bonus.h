b8c4992de698c906ae71505424c8ab4bbb536a59
dmatavel <matavelidanilo@gmail.com>
2022-12-08 12:58:46 -0300

    fix header protection in bonus header file

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,12 +6,12 @@
 /*   By: dmatavel <dmatavel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 18:39:55 by dmatavel          #+#    #+#             */
-/*   Updated: 2022/12/06 23:16:50 by dmatavel         ###   ########.fr       */
+/*   Updated: 2022/12/08 12:57:23 by dmatavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
@@ -30,4 +30,4 @@ char	*ft_strjoin(char *s1, char *s2);
 size_t	ft_strlen(const char *s);
 int		find_newline(char *s);
 
-#endif //GET_NEXT_LINE
+#endif //GET_NEXT_LINE_BONUS_H
